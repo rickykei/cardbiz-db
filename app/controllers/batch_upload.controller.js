244f526d1d3d6814bd1200da62a6e8f967b6e811
abc <rickykei@yahoo.com.hk>
2023-10-02 01:36:58 +0800

    20231002
    staff batch upload api

--- app/controllers/batch_upload.controller.js
+++ app/controllers/batch_upload.controller.js
@@ -5,6 +5,8 @@ const excel = require("exceljs");
 const Action_log = db.action_log;
 const Staff_log = db.staff_log;
 const Staff = db.staffs;
+const BeaCompanyId="64f37df6528e09409ddab475";
+const BeaUid="64f49b8bd7517dffb354c33e";
 
 exports.uploadStaffExcel =  async (req, res) => {
 	try {
@@ -812,10 +814,315 @@ exports.downloadStaffExcel =  (req, res) => {
 
 exports.uploadStaffJson =  async (req, res) => {
  try{
-	 console.log(req.body); 
-	 res.json(req.body);
-	 res.send({message: "done"});
-	 
+	 //console.log(req.body[0]); 
+	 console.log("count of staff records:");
+	 console.log(Object.keys(req.body).length);
+	 let total_of_records= Object.keys(req.body).length;
+	  var staffs = [];
+	 var xls_staffs = [];
+      var new_staffs = [];
+	  var old_staffs = [];
+ 
+	for ( i in req.body ){
+		  var query ={};
+		   query.company_id =  ObjectId(BeaCompanyId);
+		   query.staff_no =  req.body[i].staff_no;
+		   req.body[i].updatedAt = Date.now();
+		let mongoDocument =  await Staff.findOne(query).exec();
+		
+		if (mongoDocument!=undefined)
+		{
+			 let s=req.body[i];
+			
+			old_staffs.push(s);
+			console.log("old doc id"+mongoDocument.id);
+			
+			//batch update excel staff one by one
+			Staff.findByIdAndUpdate(mongoDocument.id, s, {new: true, useFindAndModify: true })
+			.then(data => {
+					if (!data) {
+						res.status(404).send({
+						  message: `Cannot update Staff with id=${id}. Maybe Staff was not found!`
+						});
+					} else  {
+				 
+						//write action log for those updated staff by batchuploader
+						const actionLog = new Action_log({
+							action: "Batch Update Staff Records",
+							log: data.fname,
+							company_id: data.company_id,
+							staff_id: data.id,
+							createdBy: data.createdBy,
+							color: "border-theme-1",
+						});
+						
+						actionLog.save(actionLog)
+						.then(data2 => {
+							if (!data2){
+								res.status(404).send({
+									message: `Cannot update Staff with id=${id}. Maybe Staff was not found!`
+									});
+							}else{
+								//backup old staff records to table staff_logs
+								console.log("actionLog save for edit");
+								 
+								console.log(data);
+								staff_log = new Staff_log({
+									action_log_id: ObjectId(data2.id),
+									staff_id: ObjectId(data._id),
+									udid:data.udid,
+									
+									company_id: data.company_id,
+									staff_no: data.staff_no,
+									 company_name_option:data.company_name_option,
+									 
+									company_name_eng2: data.company_name_eng,
+									company_name_chi2: data.company_name_chi,
+									company_name_eng3: data.company_name_eng,
+									company_name_chi3: data.company_name_chi,
+									fname: data.fname,
+									lname: data.lname,
+									  cc_no: data.cc_no,
+									  
+									  title_eng: data.title_eng,
+									  title_chi: data.title_chi,
+									    title_eng2: data.title_eng2,
+									  title_chi2: data.title_chi2,
+									  pro_title: data.pro_title,
+									  division_eng: data.division_eng,
+									  division_chi: data.division_chi,
+									  dept_eng: data.dept_eng,
+									  dept_chi: data.dept_chi,
+									  address_eng: data.address_eng,
+									  address_chi: data.address_chi,
+									  address_eng2: data.address_eng2,
+									  address_chi2: data.address_chi2,
+									  work_tel: data.work_tel,
+									  work_tel2: data.work_tel2,
+									  work_tel3: data.work_tel3,
+									  direct_tel: data.direct_tel,
+									  direct_tel2: data.direct_tel2,
+									  direct_tel3: data.direct_tel3,
+									  mobile: data.mobile,
+									  mobile2: data.mobile2,
+									  mobile3: data.mobile3,
+									  mobile_china_tel: data.mobile_china_tel,
+									  mobile_china_tel2: data.mobile_china_tel2,
+									  mobile_china_tel3: data.mobile_china_tel3,
+									  fax: data.fax,
+									  swift_no: data.swift_no,
+									  work_email: data.work_email,
+									  work_email2: data.work_email2,
+									  work_email3: data.work_email2,
+									  web_link: data.web_link,
+									  web_link_label: data.web_link_label,
+									  web_link2: data.web_link2,
+									  web_link_label2: data.web_link_label2,
+									  web_link3: data.web_link3,
+									  web_link_label3: data.web_link_label3,
+									  agent_no: data.agent_no,
+									  insurance_no: data.insurance_no,
+									  mpf_no: data.mpf_no,
+									  hkma_no: data.hkma_no,
+									  type1_no: data.type1_no,
+									  type4_no: data.type4_no,
+									  type6_no: data.type6_no,
+									  type9_no: data.type9_no,
+									  reuters_code: data.reuters_code,
+									  bloomberg_info: data.bloomberg_info,
+									  sfc_no: data.sfc_no,
+									  sfc_type1_no: data.sfc_type1_no,
+									  sfc_type2_no: data.sfc_type2_no,
+									  field051: data.field051,
+									  field052: data.field052,
+									  field053: data.field053,
+									  field054: data.field054,
+									  field055: data.field055,
+									  field056: data.field056,
+									  field057: data.field057,
+									  field058: data.field058,
+									  field059: data.field059,
+									  field060: data.field060,
+									  field061: data.field061,
+									  field062: data.field062,
+									  field063: data.field063,
+									  field064: data.field064,
+									  field065: data.field065,
+									  field066: data.field066,
+									  field067: data.field067,
+									  field068: data.field068,
+									  field069: data.field069,
+									  field070: data.field070, 
+									  additional_info: data.additional_info,									 
+									  
+									  smartcard_uid: data.smartcard_uid,
+									  bizcard_option: data.bizcard_option,
+									   
+									  qrcode_option: data.qrcode_option,
+									  profile_counter: data.profile_counter,
+									  vcf_counter: data.vcf_counter,
+									  status: data.status, 
+									  updatedBy: ObjectId(BeaUid), 
+									  createdBy: data.createdBy, 
+									  createdAt: data.createdAt, 
+									  updatedAt: Date.now(),
+								});
+								  
+								staff_log.save(staff_log);
+							//backup old staff records to table staff_logs
+							}
+						});
+						 //white action log before send successful
+					}
+			});
+		}else{
+			 
+			new_staffs.push(s);
+			console.log("new doc id");
+			
+			 // Save Staff in the database
+			 var staff=new Staff(s);
+			staff.save(s)
+				.then(data => {
+					 //white action log before send successfully
+					 const actionLog = new Action_log({
+						action: "Batch Create Staff",
+						log: data.fname,
+						company_id: data.company_id,
+						staff_id: data.id,
+						createdBy: data.createdBy,
+						color: "border-theme-1",
+					});
+					
+					actionLog.save(actionLog).then(data2 => {
+										if (!data2){
+											res.status(404).send({
+												message: `Cannot update Staff with id=${id}. Maybe Staff was not found!`
+												});
+										}else{
+					 //white action log before send successfully
+					 
+								//backup old staff records to table staff_logs
+											console.log("actionLog save for create");
+											 
+											console.log(data);
+											staff_log = new Staff_log({
+												action_log_id: ObjectId(data2.id),
+												staff_id: ObjectId(data._id),
+												udid:data.udid,
+												company_id: data.company_id,
+												staff_no: data.staff_no,
+												company_name_option:data.company_name_option,
+											 
+												company_name_eng2: data.company_name_eng,
+												company_name_chi2: data.company_name_chi,
+												company_name_eng3: data.company_name_eng,
+												company_name_chi3: data.company_name_chi,
+												fname: data.fname,
+												lname: data.lname,
+												  cc_no: data.cc_no,
+												  
+												  title_eng: data.title_eng,
+												  title_chi: data.title_chi,
+												  title_eng2: data.title_eng2,
+												  title_chi2: data.title_chi2,
+												  pro_title: data.pro_title,
+												  division_eng: data.division_eng,
+												  division_chi: data.division_chi,
+												  dept_eng: data.dept_eng,
+												  dept_chi: data.dept_chi,
+												  address_eng: data.address_eng,
+												  address_chi: data.address_chi,
+												  address_eng2: data.address_eng2,
+												  address_chi2: data.address_chi2,
+												  work_tel: data.work_tel,
+												  work_tel2: data.work_tel2,
+												  work_tel3: data.work_tel3,
+												  direct_tel: data.direct_tel,
+												  direct_tel2: data.direct_tel2,
+												  direct_tel3: data.direct_tel3,
+												  mobile: data.mobile,
+												  mobile2: data.mobile2,
+												  mobile3: data.mobile3,
+												  mobile_china_tel: data.mobile_china_tel,
+												  mobile_china_tel2: data.mobile_china_tel2,
+												  mobile_china_tel3: data.mobile_china_tel3,
+												  fax: data.fax,
+												  swift_no: data.swift_no,
+												  work_email: data.work_email,
+												  work_email2: data.work_email2,
+												  work_email3: data.work_email2,
+												  web_link: data.web_link,
+												   web_link_label: data.web_link_label,
+												  web_link2: data.web_link2,
+												  web_link_label2: data.web_link_label2,
+												  web_link3: data.web_link3,
+												  web_link_label3: data.web_link_label3,
+												  agent_no: data.agent_no,
+												  insurance_no: data.insurance_no,
+												  mpf_no: data.mpf_no,
+												  hkma_no: data.hkma_no,
+												  type1_no: data.type1_no,
+												  type4_no: data.type4_no,
+												  type6_no: data.type6_no,
+												   type9_no: data.type9_no,
+												  reuters_code: data.reuters_code,
+												  bloomberg_info: data.bloomberg_info,
+												  sfc_no: data.sfc_no,
+												   sfc_type1_no: data.sfc_type1_no,
+												  sfc_type2_no: data.sfc_type2_no,
+												  field051: data.field051,
+												  field052: data.field052,
+												  field053: data.field053,
+												  field054: data.field054,
+												  field055: data.field055,
+												  field056: data.field056,
+												  field057: data.field057,
+												  field058: data.field058,
+												  field059: data.field059,
+												  field060: data.field060,
+												  field061: data.field061,
+												  field062: data.field062,
+												  field063: data.field063,
+												  field064: data.field064,
+												  field065: data.field065,
+												  field066: data.field066,
+												  field067: data.field067,
+												  field068: data.field068,
+												  field069: data.field069,
+												  field070: data.field070, 
+												  additional_info: data.additional_info,	
+												  
+												  smartcard_uid: data.smartcard_uid,
+												  bizcard_option: data.bizcard_option,
+												  
+												  qrcode_option: data.qrcode_option,
+												  profile_counter: data.profile_counter,
+												  vcf_counter: data.vcf_counter,
+												 
+												  status: data.status, 
+												  updatedBy: ObjectId(BeaUid), 
+												  createdBy: data.createdBy, 
+												  createdAt: data.createdAt, 
+												  updatedAt: Date.now(),
+											});
+											console.log("copy staff_log");
+											  console.log(staff_log);
+											staff_log.save(staff_log);
+										
+										//backup old staff records to table staff_logs
+										}
+										});
+				 
+				})
+		}
+	  }
+	
+	console.log("new"+new_staffs.length);
+	console.log("old"+old_staffs.length);
+	  	  res.send({message: "done",old_staffs,new_staffs});
+   
+   
    
   } catch (error) {
     console.log(error);
