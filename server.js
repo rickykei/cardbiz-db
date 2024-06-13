42d8819becda9125e0bc4b74b1d4b330e1ee29c9
abc <rickykei@yahoo.com.hk>
2024-06-13 21:39:10 +0800

    20240613

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ var corsOptions = {
 app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
-app.use(express.json());
+app.use(express.json({limit: '5mb'}));
+
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
