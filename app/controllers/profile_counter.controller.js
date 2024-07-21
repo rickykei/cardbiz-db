1875bbe7a3a9eb90e8a9147f8febc1c0d9739f7b
abc <rickykei@yahoo.com.hk>
2024-07-22 00:07:27 +0800

    20240722 admin ip log

--- app/controllers/profile_counter.controller.js
+++ app/controllers/profile_counter.controller.js
@@ -157,6 +157,7 @@ exports.downloadStaffLogExcel =  (req, res) => {
   console.log("entered Profile_counter.downloadStaffLogExcel");
   const populate=['staff_id'];
     let query={};
+	let nfc=0;
 	 const { company_id, uid  } = req.query;
 	if (company_id == undefined || company_id =="" || uid=="" || uid == undefined) {
 		  return res.status(400).send("ERROR");
@@ -169,7 +170,7 @@ exports.downloadStaffLogExcel =  (req, res) => {
 	 
 		console.log("non nfc");
 	 }else{
-		  
+		  nfc=1;
 		 console.log("nfc");
 	 }
 	 
@@ -190,7 +191,7 @@ exports.downloadStaffLogExcel =  (req, res) => {
   
 			
 			
-			
+			if (nfc==0){
 			profCnts.push({
 				  updatedAtDate: updateDate[0],
 			  updatedAtTime: updateDate[1],
@@ -208,7 +209,30 @@ exports.downloadStaffLogExcel =  (req, res) => {
 			  department: obj.staff_id?.department==undefined?"":obj.staff_id.department,
 			  country: obj.staff_id?.country==undefined?"":obj.staff_id.country,
 			  
-		  });
+		 	 });
+			}else{
+				profCnts.push({
+					updatedAtDate: updateDate[0],
+				updatedAtTime: updateDate[1],
+							  company_name_eng: obj.staff_id?.company_name_eng==undefined?"":obj.staff_id.company_name_eng,
+				company_name_chi: obj.staff_id?.company_name_chi==undefined?"":obj.staff_id.company_name_chi,
+				fname: obj.staff_id?.fname==undefined?"":obj.staff_id.fname,
+				lname: obj.staff_id?.lname==undefined?"":obj.staff_id.lname,
+				position: obj.staff_id?.position==undefined?"":obj.staff_id.position,
+				address: obj.staff_id?.address==undefined?"":obj.staff_id.address,
+				address2: obj.staff_id?.address2==undefined?"":obj.staff_id.address2,
+				address3: obj.staff_id?.address3==undefined?"":obj.staff_id.address3,
+				address4: obj.staff_id?.address4==undefined?"":obj.staff_id.address4,
+				staff_no: obj.staff_id?.staff_no==undefined?"":obj.staff_id.staff_no,
+				division : obj.staff_id?.division==undefined?"":obj.staff_id.division,
+				department: obj.staff_id?.department==undefined?"":obj.staff_id.department,
+				country: obj.staff_id?.country==undefined?"":obj.staff_id.country,
+				ip: obj.ip==undefined?"":obj.ip,
+				user_agent:obj.user_agent==undefined?"":obj.user_agent,
+					});
+		  	}
+
+
 		});
 			  
 			  
@@ -216,6 +240,8 @@ exports.downloadStaffLogExcel =  (req, res) => {
 		let workbook = new excel.Workbook();
 		let worksheet = workbook.addWorksheet("staffprofilelog");
 
+
+		if (nfc==0){
 		worksheet.columns = [
 		 
 			  { header: "updatedAtDate", key: "updatedAtDate", width: 25 },
@@ -234,7 +260,28 @@ exports.downloadStaffLogExcel =  (req, res) => {
 		  { header: "department", key: "department", width: 25 },
 		  { header: "country", key: "country", width: 25 },
 		];
-
+		}else{
+			worksheet.columns = [
+		 
+				{ header: "updatedAtDate", key: "updatedAtDate", width: 25 },
+			{ header: "updatedAtTime", key: "updatedAtTime", width: 25 },
+			{ header: "company_name_eng", key: "company_name_eng", width: 25 },
+			{ header: "company_name_chi", key: "company_name_chi", width: 25 },
+			{ header: "english_name", key: "fname", width: 25 },
+			{ header: "chinese_name", key: "lname", width: 25 },
+			{ header: "position", key: "position", width: 25 },
+			{ header: "address", key: "address", width: 25 },
+			{ header: "address2", key: "address2", width: 25 },
+			{ header: "address3", key: "address3", width: 25 },
+			{ header: "address4", key: "address4", width: 25 },
+			{ header: "staff_no", key: "staff_no", width: 25 },
+			{ header: "division ", key: "division", width: 25 },
+			{ header: "department", key: "department", width: 25 },
+			{ header: "country", key: "country", width: 25 },
+			{ header: "ip", key: "ip", width: 25 },
+			{ header: "user_agent", key: "user_agent", width: 25 },
+		  ];
+		}
 		// Add Array Rows
 		worksheet.addRows(profCnts);
 
--- app/controllers/vcf_counter.controller.js
+++ app/controllers/vcf_counter.controller.js
@@ -126,6 +126,7 @@ exports.findAll = (req, res) => {
 exports.downloadStaffLogExcel =  (req, res) => {
   console.log("entered  Vcf_counter.downloadStaffLogExcel");
   const populate=['staff_id'];
+  let nfc=0;
   let query={};
 	 const { company_id, uid  } = req.query;
 	if (company_id == undefined || company_id =="" || uid=="" || uid == undefined) {
@@ -138,7 +139,7 @@ exports.downloadStaffLogExcel =  (req, res) => {
 	 
 		console.log("non nfc");
 	 }else{
-		  
+		  nfc=1;
 		 console.log("nfc");
 	 }
 	 
@@ -154,54 +155,102 @@ exports.downloadStaffLogExcel =  (req, res) => {
 			
 			 
 			let updateDate=obj.updatedAt.split(' ');
-			vcfCnts.push({
-						  updatedAtDate: updateDate[0],
-			  updatedAtTime: updateDate[1],
-			    company_name_eng: obj.staff_id?.company_name_eng==undefined?"":obj.staff_id.company_name_eng,
-			  company_name_chi: obj.staff_id?.company_name_chi==undefined?"":obj.staff_id.company_name_chi,
-			  fname: obj.staff_id?.fname==undefined?"":obj.staff_id.fname,
-			  lname: obj.staff_id?.lname==undefined?"":obj.staff_id.lname,
-			  position: obj.staff_id?.position==undefined?"":obj.staff_id.position,
-			  address: obj.staff_id?.address==undefined?"":obj.staff_id.address,
-			  address2: obj.staff_id?.address2==undefined?"":obj.staff_id.address2,
-			  address3: obj.staff_id?.address3==undefined?"":obj.staff_id.address3,
-			  address4: obj.staff_id?.address4==undefined?"":obj.staff_id.address4,
-			  staff_no: obj.staff_id?.staff_no==undefined?"":obj.staff_id.staff_no,
-			  division : obj.staff_id?.division==undefined?"":obj.staff_id.division,
-			  department: obj.staff_id?.department==undefined?"":obj.staff_id.department,
-			  country: obj.staff_id?.country==undefined?"":obj.staff_id.country,
-			  ip: obj.ip==undefined?"":obj.ip,
-			  user_agent:obj.user_agent==undefined?"":obj.user_agent,
-			  
-		  });
+
+			if (nfc==0)
+			{
+					vcfCnts.push({
+								updatedAtDate: updateDate[0],
+					updatedAtTime: updateDate[1],
+						company_name_eng: obj.staff_id?.company_name_eng==undefined?"":obj.staff_id.company_name_eng,
+					company_name_chi: obj.staff_id?.company_name_chi==undefined?"":obj.staff_id.company_name_chi,
+					fname: obj.staff_id?.fname==undefined?"":obj.staff_id.fname,
+					lname: obj.staff_id?.lname==undefined?"":obj.staff_id.lname,
+					position: obj.staff_id?.position==undefined?"":obj.staff_id.position,
+					address: obj.staff_id?.address==undefined?"":obj.staff_id.address,
+					address2: obj.staff_id?.address2==undefined?"":obj.staff_id.address2,
+					address3: obj.staff_id?.address3==undefined?"":obj.staff_id.address3,
+					address4: obj.staff_id?.address4==undefined?"":obj.staff_id.address4,
+					staff_no: obj.staff_id?.staff_no==undefined?"":obj.staff_id.staff_no,
+					division : obj.staff_id?.division==undefined?"":obj.staff_id.division,
+					department: obj.staff_id?.department==undefined?"":obj.staff_id.department,
+					country: obj.staff_id?.country==undefined?"":obj.staff_id.country,
+				 
+					
+				});
+			}else{
+						vcfCnts.push({
+						updatedAtDate: updateDate[0],
+						updatedAtTime: updateDate[1],
+						company_name_eng: obj.staff_id?.company_name_eng==undefined?"":obj.staff_id.company_name_eng,
+						company_name_chi: obj.staff_id?.company_name_chi==undefined?"":obj.staff_id.company_name_chi,
+						fname: obj.staff_id?.fname==undefined?"":obj.staff_id.fname,
+						lname: obj.staff_id?.lname==undefined?"":obj.staff_id.lname,
+						position: obj.staff_id?.position==undefined?"":obj.staff_id.position,
+						address: obj.staff_id?.address==undefined?"":obj.staff_id.address,
+						address2: obj.staff_id?.address2==undefined?"":obj.staff_id.address2,
+						address3: obj.staff_id?.address3==undefined?"":obj.staff_id.address3,
+						address4: obj.staff_id?.address4==undefined?"":obj.staff_id.address4,
+						staff_no: obj.staff_id?.staff_no==undefined?"":obj.staff_id.staff_no,
+						division : obj.staff_id?.division==undefined?"":obj.staff_id.division,
+						department: obj.staff_id?.department==undefined?"":obj.staff_id.department,
+						country: obj.staff_id?.country==undefined?"":obj.staff_id.country,
+						ip: obj.ip==undefined?"":obj.ip,
+						user_agent:obj.user_agent==undefined?"":obj.user_agent,
+
+						});
+			}
 		});
-			  
-			  
-			  //gen excel
+			   
+		//gen excel
 		let workbook = new excel.Workbook();
 		let worksheet = workbook.addWorksheet("staffvcflog");
 
-		worksheet.columns = [
-		 
-		 	  { header: "updatedAtDate", key: "updatedAtDate", width: 25 },
-		  { header: "updatedAtTime", key: "updatedAtTime", width: 25 },
-		  { header: "company_name_eng", key: "company_name_eng", width: 25 },
-		  { header: "company_name_chi", key: "company_name_chi", width: 25 },
-			  { header: "english_name", key: "fname", width: 25 },
-		  { header: "chinese_name", key: "lname", width: 25 },
-		  { header: "position", key: "position", width: 25 },
-		  { header: "address", key: "address", width: 25 },
-		  { header: "address2", key: "address2", width: 25 },
-		  { header: "address3", key: "address3", width: 25 },
-		  { header: "address4", key: "address4", width: 25 },
-		  { header: "staff_no", key: "staff_no", width: 25 },
-		  { header: "division ", key: "division", width: 25 },
-		  { header: "department", key: "department", width: 25 },
-		  { header: "country", key: "country", width: 25 },
-		  { header: "ip", key: "ip", width: 25 },
-		  { header: "user_agent", key: "user_agent", width: 25 },
-	 
-		];
+		if (nfc==0){
+
+				
+				worksheet.columns = [
+				
+					{ header: "updatedAtDate", key: "updatedAtDate", width: 25 },
+				{ header: "updatedAtTime", key: "updatedAtTime", width: 25 },
+				{ header: "company_name_eng", key: "company_name_eng", width: 25 },
+				{ header: "company_name_chi", key: "company_name_chi", width: 25 },
+					{ header: "english_name", key: "fname", width: 25 },
+				{ header: "chinese_name", key: "lname", width: 25 },
+				{ header: "position", key: "position", width: 25 },
+				{ header: "address", key: "address", width: 25 },
+				{ header: "address2", key: "address2", width: 25 },
+				{ header: "address3", key: "address3", width: 25 },
+				{ header: "address4", key: "address4", width: 25 },
+				{ header: "staff_no", key: "staff_no", width: 25 },
+				{ header: "division ", key: "division", width: 25 },
+				{ header: "department", key: "department", width: 25 },
+				{ header: "country", key: "country", width: 25 },
+			 
+			
+				];
+		}else{
+
+			worksheet.columns = [
+				
+				{ header: "updatedAtDate", key: "updatedAtDate", width: 25 },
+			{ header: "updatedAtTime", key: "updatedAtTime", width: 25 },
+			{ header: "company_name_eng", key: "company_name_eng", width: 25 },
+			{ header: "company_name_chi", key: "company_name_chi", width: 25 },
+				{ header: "english_name", key: "fname", width: 25 },
+			{ header: "chinese_name", key: "lname", width: 25 },
+			{ header: "position", key: "position", width: 25 },
+			{ header: "address", key: "address", width: 25 },
+			{ header: "address2", key: "address2", width: 25 },
+			{ header: "address3", key: "address3", width: 25 },
+			{ header: "address4", key: "address4", width: 25 },
+			{ header: "staff_no", key: "staff_no", width: 25 },
+			{ header: "division ", key: "division", width: 25 },
+			{ header: "department", key: "department", width: 25 },
+			{ header: "country", key: "country", width: 25 },
+			{ header: "ip", key: "ip", width: 25 },
+			{ header: "user_agent", key: "user_agent", width: 25 },
+			];
+		}
 
 		// Add Array Rows
 		worksheet.addRows(vcfCnts);
